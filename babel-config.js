231db6428fcc6188cf545d6bdd202e9d3f4fcc74
Brian Ng <bng412@gmail.com>
2016-11-29 23:43:26 -0600

    Ensure babel extends builtins on jsnext build

---
+++ babel-config.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var plugins = [
+  [
+    "babel-plugin-transform-builtin-extend",
+    {
+      "globals": ["Error"],
+      "approximate": true
+    }
+  ]
+];
+
+var env = process.env.BABEL_ENV || process.env.NODE_ENV;
+
+var modules;
+
+if (env === 'es') {
+  modules = false;
+} else {
+  modules = 'commonjs';
+  plugins.push('add-module-exports');
+}
+
+module.exports = {
+  "presets": [
+    ["es2015", { modules: modules }]
+  ],
+  "plugins": plugins
+};
